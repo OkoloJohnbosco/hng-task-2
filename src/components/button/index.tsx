f4b76f6f82cef591a0efd0b5b1e4368bad9659ab
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-11-05 06:04:13 +0100

    feat: created the header section of the contact page

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,13 +3,17 @@ import styles from "./button.module.css";
 function Button({
   children,
   type = "button",
+  variant = "primary",
 }: {
   children: string;
   type?: "button" | "submit" | "reset";
+  variant?: "primary" | "secondary";
 }) {
   return (
-    <div className={styles.form_group}>
-      <button type={type}>{children}</button>
+    <div className={styles.button_wrapper}>
+      <button data-btn={variant} type={type}>
+        {children}
+      </button>
     </div>
   );
 }
--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import Button from "../../components/button";
 import Input from "../../components/input";
+import styles from "./contact.module.css";
 
 function Contact() {
   return (
-    <div>
-      <Input label="First Name" placeholder="Enter your first name" />
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className={styles.main_header}>
+          <h2>Contact Me</h2>
+          <p>Hi there, contact me to ask me about anything you have in mind.</p>
+        </div>
+        <Input label="First Name" placeholder="Enter your first name" />
+        <Button>Submit</Button>
+      </main>
     </div>
   );
 }
