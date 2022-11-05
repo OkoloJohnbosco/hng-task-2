16f614b7e7939dd3564b629110d061056a9d7a0f
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-11-05 02:32:36 +0100

    feat: installed and configured react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/home";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 const App: React.FC = () => {
-  return <Home />;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
