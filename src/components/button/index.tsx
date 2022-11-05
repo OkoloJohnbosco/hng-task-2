0b51834a1b29d551f91917a4333261f65f3d69da
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-11-05 06:35:31 +0100

    style: styled the button component for for different button states such as disabled state

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -11,7 +11,7 @@ function Button({
 }) {
   return (
     <div className={styles.button_wrapper}>
-      <button data-btn={variant} type={type}>
+      <button data-btn={variant} type={type} disabled>
         {children}
       </button>
     </div>
